fe994230505698d109894c87a669f844c9abcbde
Swethasanmugam <130545765+Swethasanmugam@users.noreply.github.com>
2024-09-11 12:47:33 +0530

    Add files via upload

---
+++ bhuvana1.py
@@ -0,0 +1,10 @@
+def swap (a,b):
+    a,b=b,a
+    print("after swapping")
+    print("a=",a)
+    print("b=",b)
+a=input("enter the 1st number")
+b=input("enter the 2nd number")
+print("a=",a)
+print("b=",b)
+swap(a,b)
